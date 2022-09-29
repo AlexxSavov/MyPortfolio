4870b63d42af239791f98cfe4632eb922b474865
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-09-29 12:14:42 -0400

    adding my about component js to render some infomation about me

---
+++ src/components/About/index.js
@@ -0,0 +1,35 @@
+function About() {
+    return(
+    <section className="title">
+      <h1 class="name">about: Alexandre Savov</h1>
+      <hr></hr>
+      
+      <div className="row justify-content-center">
+        <div className="col-10" id="about-section">
+            <img class="mb-5" 
+            src="https://github.com/AlexxSavov/MyPortfolio/raw/main/src/assets/images/Alex-Savov.jpg"
+             alt="Alexandre Savov"/>
+        <p>
+            Insert some info about myself here
+        </p>
+        <p>
+        Insert some info about myself here
+        </p>
+        <p>
+        Insert some info about myself here
+        </p>
+        <p>
+        Insert some info about myself here
+        </p>
+        <p>
+        Insert some info about myself here
+      </p>
+      <p>
+            Take a Closer Look <a href="#/resume" class="link">RESUME</a>
+    </p>
+      </div>  
+      </div>
+    </section> 
+)}
+  
+export default About;
