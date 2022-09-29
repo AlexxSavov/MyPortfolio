93507e29c5b0a1597929a1e02eebf7348c28b913
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-09-29 12:35:54 -0400

    defined set error msg o it can actualy work

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,6 +5,7 @@ function ContactForm() {
 
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
+    const [errorMessage, setErrorMessage] = useState('');
 
     const { name, email, message } = formState;
 
