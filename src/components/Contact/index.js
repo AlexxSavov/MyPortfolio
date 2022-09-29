10fe5f96062473c7cf2ea0c51984ae784941021a
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-09-29 12:34:27 -0400

    addig function to validate the email adress using my helper

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,6 +8,29 @@ function ContactForm() {
 
     const { name, email, message } = formState;
 
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+    
+                if(!isValid) {
+                    setErrorMessage('You entered an invalid Email, please enter a valid Email');
+                } else {
+                    setErrorMessage('');
+                }
+
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                } 
+        }
+
+        if (!errorMessage) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+        }
+    } 
+
     function handleSubmit(e) {
         e.preventDefault();
     }
