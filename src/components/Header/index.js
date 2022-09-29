c76806d5a0711083068509bcf20217e23c5b68be
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-09-29 10:53:17 -0400

    importing components into header js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
 import {Route, Redirect, HashRouter} from "react-router-dom";
+import Navigation from '../../components/Navigation';
+import About from '../../components/About';
+import Contact from '../../components/Contact';
+import Portfolio from '../../components/Portfolio';
+import Resume from '../../components/Resume'; 
 
 
 class Header extends Component {   
